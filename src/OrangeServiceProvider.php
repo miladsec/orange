bfa65ce20847abeb8b12daf71282e05c3ad46022
Milad Dehghan <milawddehghan@gmail.com>
2020-10-30 11:46:02 +0330

    serviceProcider

--- src/OrangeServiceProvider.php
+++ src/OrangeServiceProvider.php
@@ -8,9 +8,12 @@ class OrangeServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        $this->publishes([
-            __DIR__ . '/../config/orange.php' => config_path('orange.php')
-        ]);
+        if ($this->app->runningInConsole()) {
+            $this->publishes([
+                __DIR__ . '/../config/orange.php' => config_path('orange.php'),
+                __DIR__ . '/../database/migrations/create_oranges_table.php.stub' => database_path('migrations/' . '2020_10_30_064244_create_oranges_table.php')
+            ]);
+        }
     }
 
     public function register()
